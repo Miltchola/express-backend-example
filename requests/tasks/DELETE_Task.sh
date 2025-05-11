3919b71848cc3815b5f73d1ff36fc7f2196be47c
Milton Kiefer <145204997+Miltchola@users.noreply.github.com>
2025-05-11 22:33:16 +0000

    atualização

--- requests/tasks/DELETE_Task.sh
+++ requests/tasks/DELETE_Task.sh
@@ -1,3 +1,3 @@
 curl --request DELETE \
-  --url http://localhost:3000/tasks/6814e5ded37c36d73fe9a0e9 \
+  --url http://localhost:3000/tasks/681cc57c8c4750dfec7c1acc \
   --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTRlNWRlZDM3YzM2ZDczZmU5YTBlOSIsImlhdCI6MTc0NzAwMDIzNywiZXhwIjoxNzQ3MDAzODM3fQ.cK1DZnDFWcpZol0ZEGwTzIVTSV76FACh_b5LFLTbw3k"
