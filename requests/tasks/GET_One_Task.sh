3685667fbeb29ca08714114f2971ef5e40d367c0
Milton Kiefer <145204997+Miltchola@users.noreply.github.com>
2025-05-11 22:22:53 +0000

    atualização

--- requests/tasks/GET_One_Task.sh
+++ requests/tasks/GET_One_Task.sh
@@ -1,3 +1,3 @@
 curl --request GET \
   --url http://localhost:3000/tasks/681cc57c8c4750dfec7c1acc \
-  --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTRlNWRlZDM3YzM2ZDczZmU5YTBlOSIsImlhdCI6MTc0NjcxNTY3MCwiZXhwIjoxNzQ2NzE5MjcwfQ.o4llFCHsKNnEOW3jlfQBEN7SsPpB5EBZIQ-fRZKFaM0"
+  --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTRlNWRlZDM3YzM2ZDczZmU5YTBlOSIsImlhdCI6MTc0NzAwMDIzNywiZXhwIjoxNzQ3MDAzODM3fQ.cK1DZnDFWcpZol0ZEGwTzIVTSV76FACh_b5LFLTbw3k"
--- requests/tasks/POST_Error_Bad_request.sh
+++ requests/tasks/POST_Error_Bad_request.sh
@@ -1,7 +1,7 @@
 curl --request POST \
   --url http://localhost:3000/tasks \
   --header "Content-Type: application/json" \
-  --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTRlNWRlZDM3YzM2ZDczZmU5YTBlOSIsImlhdCI6MTc0NjcxNTY3MCwiZXhwIjoxNzQ2NzE5MjcwfQ.o4llFCHsKNnEOW3jlfQBEN7SsPpB5EBZIQ-fRZKFaM0" \
+  --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTRlNWRlZDM3YzM2ZDczZmU5YTBlOSIsImlhdCI6MTc0NzAwMDIzNywiZXhwIjoxNzQ3MDAzODM3fQ.cK1DZnDFWcpZol0ZEGwTzIVTSV76FACh_b5LFLTbw3k" \
   --data '{
     "description": ""
   }'
