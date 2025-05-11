25ae13a13db4bbf98aa41c947ef47b77089098de
Milton Kiefer <145204997+Miltchola@users.noreply.github.com>
2025-05-11 22:11:34 +0000

    feat: update name

--- requests/tasks/POST_Task.sh
+++
@@ -1,8 +0,0 @@
-curl --request POST \
-  --url http://localhost:3000/tasks \
-  --header "Authorization: Bearer $1" \
-  --header 'Content-Type: application/json' \
-  --data '{
-    "title": "Terminar trabalho de sistemas",
-    "description": "Finalizar slides e estudar as falas"
-  }'
