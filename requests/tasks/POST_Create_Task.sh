153164af477efa401862474b3296fd8b6e8e8361
Milton Kiefer <145204997+Miltchola@users.noreply.github.com>
2025-05-11 21:57:08 +0000

    Feat: ERROR Simulation

--- requests/tasks/POST_Create_Task.sh
+++ requests/tasks/POST_Create_Task.sh
@@ -1,9 +1,9 @@
 curl --request POST \
   --url http://localhost:3000/tasks \
   --header "Content-Type: application/json" \
-  --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTRlNWRlZDM3YzM2ZDczZmU5YTBlOSIsImlhdCI6MTc0NjcxNTY3MCwiZXhwIjoxNzQ2NzE5MjcwfQ.o4llFCHsKNnEOW3jlfQBEN7SsPpB5EBZIQ-fRZKFaM0" \
+  --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTRlNWRlZDM3YzM2ZDczZmU5YTBlOSIsImlhdCI6MTc0NzAwMDIzNywiZXhwIjoxNzQ3MDAzODM3fQ.cK1DZnDFWcpZol0ZEGwTzIVTSV76FACh_b5LFLTbw3k" \
   --data '{
-    "title": "Finalizar Back-End",
-    "description": "Atualizar e Testar o CRUD Autenticado",
+    "title": "Enviar trabalho de FULLSTACK",
+    "description": "Gravar vídeo e concluir README.md",
     "completed": false
   }'
--- requests/tasks/POST_Task.sh
+++ requests/tasks/POST_Task.sh
@@ -3,6 +3,6 @@ curl --request POST \
   --header "Authorization: Bearer $1" \
   --header 'Content-Type: application/json' \
   --data '{
-    "title": "Finalizar Back-End",
-    "description": "Atualizar e Testar o CRUD Autenticado"
+    "title": "Terminar trabalho de sistemas",
+    "description": "Finalizar slides e estudar as falas"
   }'
